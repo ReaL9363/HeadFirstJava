0beee8cc0b343151f9fa5ea8c9ef61e7fb4570a6
real@mpower-social.com <real@mpower-social.com>
2017-05-25 18:13:10 +0600

    adding setUpGame() in DotComBust class and change dotComList to dotComsList

--- src/Ch_06_UsingTheJavaLibrary/DotComBust.java
+++ src/Ch_06_UsingTheJavaLibrary/DotComBust.java
@@ -1,8 +1,5 @@
 package Ch_06_UsingTheJavaLibrary;
 
-import Ch_05_ExtraStrengthmethods.SimpleDotComGame.GameHelper;
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +8,28 @@ import java.util.List;
  */
 public class DotComBust {
     private GameHelper helper = new GameHelper();
-    private List<DotCom> dotComList = new ArrayList<>();
+    private List<DotCom> dotComsList = new ArrayList<>();
     private int numOfGuess = 0;
+
+    private void setUpGame() {
+        // first make some dot coms and give them locations
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+        System.out.println("Your goal is to sink three dot coms.");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        } // close for loop
+    }
 
     public static void main(String[] args) {
 
